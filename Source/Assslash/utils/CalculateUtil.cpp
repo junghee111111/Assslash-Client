abd0f020649a8f0906b0c211edf59bba781a6f9a
junghee111111 <jungheejjang1004@gmail.com>
2024-11-21 01:30:00 +0900

    Initial commit

---
+++ Source/Assslash/utils/CalculateUtil.cpp
@@ -0,0 +1,74 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CalculateUtil.h"
+
+/**
+ * 
+ * @param damageAmount 
+ * @param attackTraceDir 
+ * @param impulseX 
+ * @param impulseZ 
+ */
+void UCalculateUtil::Calculate_ImpulseForce(
+	int32 damageAmount,int32 attackTraceDir, double& impulseX, double& impulseZ
+	)
+{
+	impulseX = (damageAmount * 6 + 500) * attackTraceDir;
+	impulseZ = (damageAmount * 8) + 300;
+}
+
+void UCalculateUtil::Calculate_PlayerAbilityByStat(
+	int32 STR, int32 DEX, int32 LUK, int32 SPD, int32 CON,
+	int32& Att, double& AttDistance, double& AttCritical, int32& AttMin, int32& AdditionalImpuse,
+	int32& Def, double& AttPostDelay, double& CriticalRate, double& WalkSpeed, double& AttSpeed,
+	int32& MaxHP
+	)
+{
+	// 공격력
+	if(STR>20){
+		Att = 20 + (STR-20)*2;
+	}else{
+		Att = STR;
+	}
+
+	// 최소 공격력
+	AttMin = (int)round(Att*(CON/100));
+	if(AttMin>=Att)
+	{
+		AttMin = Att-1;
+	}
+
+	// 방어력
+	if(STR>20)
+	{
+		Def = (int)round(6 + (STR)*0.8);
+	} else
+	{
+		Def = (int)round(STR*0.6);
+	}
+
+	// 최대 HP
+	MaxHP = 100 + ((STR-10)*3);
+
+	// 크리티컬 확률
+	CriticalRate = (LUK-10)/100 + 0.1;
+
+	// 크리티컬 추가 대미지
+	AttCritical = 0.5 + (LUK-10)/100;
+
+	// 공격 거리
+	AttDistance = 1 + (DEX)/200;
+
+	// 공격 후 딜레이
+	AttPostDelay = 0.4+ (0.3 - DEX/200);
+
+	// 공격 속도 (1 ~ 2)
+	AttSpeed = 0.9 + (SPD/100);
+	
+	// 이동 속도 (500~)
+	WalkSpeed = 500 + SPD*4;
+
+	// 추가 엉덩이 밀치기 (0~300)
+	AdditionalImpuse = CON * 3;
+}
---
+++ Source/Assslash/utils/CalculateUtil.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "CalculateUtil.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class ASSSLASH_API UCalculateUtil : public UObject
+{
+	GENERATED_BODY()
+
+	UFUNCTION(BlueprintPure, Category="Util|Calculator")
+	static void Calculate_ImpulseForce(int32 damageAmount, int32 attackTraceDir, double& impulseX, double& impulseZ);
+
+	UFUNCTION(BlueprintPure, Category="Util|Calculator")
+	static void Calculate_PlayerAbilityByStat(int32 STR, int32 DEX, int32 LUK, int32 SPD, int32 CON, int32& Att,
+	                                          double& AttDistance, double& AttCritical, int32& AttMin, int32& AdditionalImpuse,
+	                                          int32& Def, double& AttPostDelay, double& CriticalRate, double& WalkSpeed,
+	                                          double& AttSpeed, int32& MaxHP);
+};
